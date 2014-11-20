875a9618cd37943292125490a58721f105ff2593
Laj1985 <laj@home.nl>
2014-11-20 20:44:06 +0100

    Minor updates

--- USBWebserver v8.6/root/project Luuk/BarChartScript.js
+++ USBWebserver v8.6/root/project Luuk/BarChartScript.js
@@ -1,6 +1,7 @@
 ﻿var height = 200;
 var width = 400;
 var isDrawn = false;
+var selectedCity;
 var y;
 var x;
 
@@ -8,8 +9,9 @@ function draw(selectedCity) {
 
     if (!isDrawn) {
         var spacing = 5;
-        var margin = { top: 20, right: 30, bottom: 60, left: 40 };
+        var margin = { top: 20, right: 30, bottom: 80, left: 40 };
         var barWidth = 40;
+        this.selectedCity = selectedCity;
 
         var chart = d3.select(".chart");
         if (!chart.empty()) {
@@ -24,11 +26,10 @@ function draw(selectedCity) {
 
             height = height - margin.top - margin.bottom;
             width = width - margin.left - margin.right;
-            console.log(selectedCity.size());
             barWidth = ((width - (selectedCity.size() * spacing)) / selectedCity.size());
 
             x = d3.scale.ordinal().rangeRoundBands([0, width], 1).domain(selectedCity.keys());
-            y = d3.scale.linear().range([height, 0]).domain([0, d3.max(selectedCity.values()) / 100]);
+            y = d3.scale.linear().range([height, 0]).domain([0, 0.1]);
 
             var xAxis = d3.svg.axis().scale(x).orient("bottom");
             var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10, "%");
@@ -42,6 +43,11 @@ function draw(selectedCity) {
                 .attr("class", "x axis")
                 .attr("transform", "translate(0," + height + ")")
                 .call(xAxis)
+                .selectAll("text")
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", ".15em")
+                .attr("transform", function (d) { return "rotate(-65)" });
 
             chart.append("g")
                 .attr("class", "y axis")
@@ -63,6 +69,18 @@ function draw(selectedCity) {
                 .attr("height", function (d) { return height - y(0); })
                 .on("mousedown", barClick);
 
+            /*
+            chart.selectAll(".value")
+                .data(selectedCity.entries())
+                .enter().append("text")
+                .attr("class", "value")
+                .attr("x", function (d, index) { return x(d.key); })
+                .attr("y", function (d) { return y(selectedCity.get(d.key) / 100) + 13; })
+                .attr("fill", "white")
+                .attr("text-anchor", "middle")
+                .text(function (d) { return selectedCity.get(d.key) });
+            */
+
             isDrawn = true;
             animatedBarChartUpdate(selectedCity);
         }
@@ -72,20 +90,20 @@ function draw(selectedCity) {
 };
 
 function barClick(d) {
-    alert("De waarde is: " + d.value);
+    alert("De waarde is: " + selectedCity.get(d.key));
 };
 
 
 function animatedBarChartUpdate(selectedCity) {
 
     d3.selectAll(".bar").transition()
-            .duration(1000)
+            .duration(500)
             .attr("y", function (d) { return height - 0; })
             .attr("height", function (d) { return 0; });
 
     d3.selectAll(".bar").transition()
             .duration(1000)
-            .delay(1000)
+            .delay(500)
             .attr("y", function (d) { return y(selectedCity.get(d.key) / 100); })
             .attr("height", function (d, i, h) { return height - y(selectedCity.get(d.key) / 100); });
 };
