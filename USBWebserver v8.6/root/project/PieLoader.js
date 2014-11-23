076246bbbf33015eaaedc7b0380ef692c50aa059
Grabot <SanderKools@gmail.com>
2014-11-23 02:03:36 +0100

    pies charteses
    made some awesome pie chart.
    You have the map and with clicking you get a pie chart with info about
    marriage or some shit

---
+++ USBWebserver v8.6/root/project/PieLoader.js
@@ -0,0 +1,63 @@
+var pieData;
+var pieName;
+
+function drawPie( pieData )
+{
+
+	//piechart
+	var pieName = "Eemsmond";
+	
+
+	var pie = new d3pie("pie", 
+	{
+		header: 
+		{
+   			title: 
+   			{
+     			text: pieName,
+      			fontSize: 20
+		    }
+		},
+		data: 
+		{
+			content: pieData
+		},
+		tooltips: 
+		{
+		    enabled: true,
+		    type: "placeholder",
+		    string: "{label}: {percentage}%",
+		    styles: 
+		    {
+				fadeInSpeed: 500,
+				backgroundColor: "#111111",
+				backgroundOpacity: 0.8,
+				color: "#ffffff",
+				borderRadius: 4,
+				font: "verdana",
+				fontSize: 20,
+				padding: 20
+		    }
+  		},
+  		effects:
+  		{
+			load: 
+			{
+				speed: 500
+			},
+			pullOutSegmentOnClick: 
+			{
+				effect: "elastic",
+				speed: 200,
+				size: 40
+			}
+		},
+	});
+
+	var d = document.getElementById('pie');
+	d.style.position = "absolute";
+	d.style.left =  "700px";
+
+	//end piechart
+	return pie;
+}
