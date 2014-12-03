ac79257ff35dcfabb1be8a4591b96af39c02280d
Grabot <SanderKools@gmail.com>
2014-12-03 10:00:13 +0100

    Rapport
    laTeX awesomeness

---
+++ USBWebserver v8.6/root/project/lines.js
@@ -0,0 +1,11 @@
+var notSunday = [];
+var Sunday = [];
+
+notSunday[0] = "But go to church anyway!";
+notSunday[1] = "Why are you not in church though!?";
+notSunday[2] = "remember that Jesus is watching you";
+notSunday[3] = "Jesus would have wanted you to be in church!";
+notSunday[4] = "What time is it? Church Time!";
+notSunday[5] = "if you go to church now, you might not go to hell!";
+
+Sunday[0] = "Time to go to church!";
