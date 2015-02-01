febec5783c285d784f3541cb2af0e8c546f2477b
Grabot <SanderKools@gmail.com>
2015-02-01 14:10:30 +0100

    added bar to your stuff
    added bars shizzle

---
+++ Assignment 3/Project/drawBarChart.js
@@ -0,0 +1,13 @@
+function drawBarChart( chartData )
+{
+    var svg = dimple.newSvg("#BarChart", 600, 400);
+            
+    var chart = new dimple.chart(svg, chartData);
+    var x = chart.addCategoryAxis("x", "Disease");
+    x.addOrderRule(['Naseau', 'Lumbar Pain', 'Urine Pushing', 'Micturition pains', 'Burning of urethra', 'diagnosis inflammation of urinary bladder', 'diagnosis nephritis of renal pelvis origin']);
+    var y = chart.addMeasureAxis("y", "Percentage of people");
+    var s = chart.addSeries(null, dimple.plot.bar);
+    chart.draw(500);
+
+    return chart;
+}
