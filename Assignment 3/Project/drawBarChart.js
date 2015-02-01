556c0ff683f92b4873be3071cb40f59a9d950970
Laj1985 <laj@home.nl>
2015-02-01 17:20:52 +0100

    Slider werkend

--- Assignment 3/Project/drawBarChart.js
+++ Assignment 3/Project/drawBarChart.js
@@ -6,7 +6,7 @@ function drawBarChart( chartData )
     var x = chart.addCategoryAxis("x", "Disease");
     x.addOrderRule(['Naseau', 'Lumbar Pain', 'Urine Pushing', 'Micturition pains', 'Burning of urethra', 'diagnosis inflammation of urinary bladder', 'diagnosis nephritis of renal pelvis origin']);
     var y = chart.addMeasureAxis("y", "Percentage of people");
-    var s = chart.addSeries(null, dimple.plot.bar);
+    var s = chart.addSeries("Diagnose", dimple.plot.bar);
     chart.draw(500);
 
     return chart;
